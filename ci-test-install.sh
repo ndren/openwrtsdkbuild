f225824deb029aade0c76fb414146e64c23c9511
Andrei E <andreien@proton.me>
2023-06-22 10:21:57 +0100

    Attempt to run CI with dind

---
+++ ci-test-install.sh
@@ -0,0 +1,11 @@
+sed -i 's/https/http/g' /etc/opkg/distfeeds.conf
+sed -i '/check_signature/s/^/#/g' /etc/opkg.conf
+
+opkg update
+opkg install wget-ssl
+
+echo "src/gz myrepo http://usernameisnotimportant:${PERSONAL_ACCESS_TOKEN}@${UPLOAD_REPO}" >> /etc/opkg/customfeeds.conf
+opkg update
+opkg install "${PACK_NAME}"
+
+"${PACK_NAME}"
