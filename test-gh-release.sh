1481d85f8f575b2ab9e07c5753aae1674f2198c5
Andrei E <andreien@proton.me>
2023-07-14 12:18:42 +0100

    Add release test

---
+++ test-gh-release.sh
@@ -0,0 +1,13 @@
+sed -i 's/https/http/g' /etc/opkg/distfeeds.conf
+sed -i '/check_signature/s/^/#/g' /etc/opkg.conf
+
+mkdir -p /var/lock/ && touch /var/lock/opkg.lock
+opkg update
+opkg install curl
+
+opkg print-architecture
+
+curl "${UPLOAD_REPO}/releases/${IPK_NAME}" -o "${IPK_NAME}"
+opkg install "${IPK_NAME}"
+
+"${PACK_NAME}"
